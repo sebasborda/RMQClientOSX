54733f5ee4bbfd92e001df32d9862c9c9c9789fc
Sebastian Borda <seba@avi-on.com>
2019-05-10 16:01:12 -0300

    Initial Commit

---
+++ RMQClientTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RMQClientTest
+//
+//  Created by Sebastian Borda on 10/05/2019.
+//  Copyright © 2019 Sebastian Borda. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RMQClientTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RMQClientTest
+//
+//  Created by Sebastian Borda on 10/05/2019.
+//  Copyright © 2019 Sebastian Borda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
